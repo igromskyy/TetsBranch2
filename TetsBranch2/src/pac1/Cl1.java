cdef15f6f4d7a1620d0df0363d73eacff935a17a
iyst-01 <iyst-01@yandex.ru>
2013-10-31 21:17:15 +0200

    test 1 -2

--- TetsBranch2/src/pac1/Cl1.java
+++ TetsBranch2/src/pac1/Cl1.java
@@ -5,6 +5,7 @@ public class Cl1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test1 add line");
+		System.out.println("test1 add line1");
 
 	}
 
