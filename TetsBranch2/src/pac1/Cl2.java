dc4414ced08443cfece1afc4fa7639ac5ad16b2c
iyst-01 <iyst-01@yandex.ru>
2013-11-10 14:00:29 +0200

    y1

--- TetsBranch2/src/pac1/Cl2.java
+++ TetsBranch2/src/pac1/Cl2.java
@@ -2,12 +2,7 @@ package pac1;
 
 public class Cl2 {
 	private void sysout() {
-		System.out.println("test 1-2");
-		
-		System.out.println("test 1-1");
-		System.out.println("test 2-1");
-		
-		System.out.println("test 2-2");
+		System.out.println("y1");
 	}
 
 }
